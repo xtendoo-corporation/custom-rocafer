5b106008d4cf5c96207dd8e160a9700192396a69
manuelcalerosolis <manuelcalerosolis@gmail.com>
2025-05-28 09:01:36 +0200

    sync

--- rocafer_product_template/models/product_template.py
+++ rocafer_product_template/models/product_template.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2024 Manuel Calero (<https://xtendoo.es>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
+from math import ceil
 from odoo import fields, models, api
 
 
